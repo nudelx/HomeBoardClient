cbba703d38d22c7dc66c6a24d9487ba634c282b8
Alex Nudelman <nudelx@gmail.com>
2018-06-13 21:24:05 +0300

    local data

--- src/dataProvider/dataFetch.js
+++ src/dataProvider/dataFetch.js
@@ -3,44 +3,44 @@ export const serverFetch = () => {
   if (process.env.NODE_ENV === "development") {
     return Promise.resolve({
       ROW_0: {
-        NAME: "ROOT-PC",
-        MAC: "94-DE-80-7E-92-47",
+        NAME: "AAAAA",
+        MAC: "888888888",
         IP: "192.168.0.100",
         TIME: "Permanent"
       },
       ROW_1: {
-        NAME: "Unknown",
-        MAC: "3C-BD-D8-E7-2C-A1",
+        NAME: "BBBBBB",
+        MAC: "3233333333",
         IP: "192.168.0.110",
         TIME: "Permanent"
       },
       ROW_2: {
-        NAME: "rockrobo",
-        MAC: "34-CE-00-E8-2E-E9",
+        NAME: "CCCCCCC",
+        MAC: "344444444444",
         IP: "192.168.0.107",
         TIME: "01:34:15"
       },
       ROW_3: {
-        NAME: "Alexs-MBP",
-        MAC: "B8-E8-56-46-7E-A0",
+        NAME: "EEEEEE",
+        MAC: "B899999999999",
         IP: "192.168.0.101",
         TIME: "Permanent"
       },
       ROW_4: {
-        NAME: "Chromecast",
-        MAC: "F4-F5-D8-95-28-FA",
+        NAME: "FFFFFFFFFF",
+        MAC: "F477777777A",
         IP: "192.168.0.103",
         TIME: "01:38:33"
       },
       ROW_5: {
-        NAME: "Unknown",
-        MAC: "10-08-C1-94-9C-FE",
+        NAME: "JJJJJJJJJ",
+        MAC: "1rrrrrrrrt666E",
         IP: "192.168.0.111",
         TIME: "Permanent"
       },
       ROW_6: {
-        NAME: "NudelOnePlus_3T",
-        MAC: "C0-EE-FB-F6-34-AA",
+        NAME: "IIIIIIIIIII",
+        MAC: "Ctrtty565656",
         IP: "192.168.0.104",
         TIME: "01:39:58"
       }
