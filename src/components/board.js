6c997fb277720791db88d6e85c605437de965f5b
Alex Nudelman <nudelx@gmail.com>
2018-08-04 23:08:29 +0300

    more clean

--- src/components/board.js
+++ src/components/board.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Board = ({ children }) => (
   <div className="board">
-    {typeof children === "function" ? children() : null}
+    {typeof children === "function" ? children() : children}
   </div>
 )
 
