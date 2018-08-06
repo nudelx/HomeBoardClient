2e2adab435f0c8cad9591d731b0feef4210193a7
nudelx <alex.nudelman@samanage.com>
2018-08-06 15:17:12 +0300

    clean

--- src/dataProvider/dataFetch.js
+++ src/dataProvider/dataFetch.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import { DHCP } from './dataMock'
 export const serverFetch = () => {
-  console.log('DDDDDD => ', DHCP)
   if (process.env.NODE_ENV === 'development') {
-    console.log('DHCP', DHCP)
     return Promise.resolve(DHCP)
   } else {
     return axios.get('/?action=API').then(data => {
