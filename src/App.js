94289a26c69480ea14a01fc37afa5e0567200d46
nudelx <alex.nudelman@samanage.com>
2018-08-06 16:44:41 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -3,33 +3,8 @@ import './App.css'
 import Header from './components/header'
 import Board from './components/board'
 import Box from './components/box'
-import { diffData } from './dataProvider/dataDiff'
-import { serverFetch } from './dataProvider/dataFetch'
 
 class App extends Component {
-  state = {}
-
-  getData = () => {
-    serverFetch().then(incoming => {
-      const localData = JSON.parse(localStorage.localDataDhcp || '{}')
-      localStorage.setItem('localDataDhcp', JSON.stringify(incoming))
-      this.setState({
-        dhcp: diffData({ ...localData }, { ...incoming }),
-        traffic: {},
-        hosts: {}
-      })
-    })
-  }
-
-  componentDidMount() {
-    this.getData()
-    this.timer = setInterval(this.getData, 30000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer)
-  }
-
   render() {
     return (
       <div className="App">
@@ -37,7 +12,7 @@ class App extends Component {
         <Board>
           {() => (
             <React.Fragment>
-              <Box data={this.state.dhcp} onReload={this.getData} />
+              <Box type={'DHCP'} />
               {/* <Box data={this.state.traffic} /> */}
               {/* <Box data={this.state.hosts} /> */}
             </React.Fragment>
--- src/components/box.js
+++ src/components/box.js
@@ -1,21 +1,51 @@
-import React from "react"
-import List from "./list"
-const Box = ({ data, onReload }) => {
-  return (
-    <div className="box table">
-      <div className="box-header">
-        {data ? (
-          `home dhcp - ${Object.keys(data).length} devices`
-        ) : (
-          <div className="icon spinner" />
-        )}
-        <span className="reload-holder" onClick={onReload}>
-          <div className="reload" />
-        </span>
+import React, { Component } from 'react'
+import List from './list'
+import { serverFetch } from '../dataProvider/dataFetch'
+import { diffData } from '../dataProvider/dataDiff'
+
+class Box extends Component {
+  state = {}
+
+  getData = () => {
+    const { type } = this.props
+    serverFetch().then(incoming => {
+      const localData = JSON.parse(localStorage.localDataDhcp || '{}')
+      localStorage.setItem('localDataDhcp', JSON.stringify(incoming))
+      this.setState({
+        dhcp: diffData({ ...localData }, { ...incoming }),
+        traffic: {},
+        hosts: {}
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getData()
+    this.timer = setInterval(this.getData, 30000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  render() {
+    const { dhcp } = this.state
+    return (
+      <div className="box table">
+        <div className="box-header">
+          {dhcp ? (
+            `home dhcp - ${Object.keys(dhcp).length} devices`
+          ) : (
+            <div className="icon spinner" />
+          )}
+          <span className="reload-holder" onClick={this.getData}>
+            <div className="reload" />
+          </span>
+        </div>
+        <List data={dhcp} />
       </div>
-      <List data={data} />
-    </div>
-  )
+    )
+  }
 }
 
 export default Box
